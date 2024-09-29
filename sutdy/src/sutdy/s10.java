defb26d5e8d99ddf54e879b3571486b7af4dc717
LastProject <tjdgus5099@naver.com>
2024-09-29 11:08:09 +0900

    정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.

---
+++ sutdy/src/sutdy/s10.java
@@ -0,0 +1,13 @@
+package sutdy;
+
+class s10 {
+	public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if (num1 >= -50000 && num1 <= 50000 && num2 >= -50000 && num2 <= 50000) {
+            answer = num1 + num2;
+        }
+        
+        return answer;
+    }
+}

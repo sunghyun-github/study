d7e39a150d17da791c1cb92ae9cb0662425d6287
LastProject <tjdgus5099@naver.com>
2024-09-30 18:53:22 +0900

    정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.

---
+++ sutdy/src/sutdy/s12.java
@@ -0,0 +1,14 @@
+package sutdy;
+
+class s12 {
+	public double solution(int[] numbers) {
+        double answer = 0;
+        
+        for (int i = 0; i < numbers.length; i++){
+            answer = answer + numbers[i];
+        }
+        answer = answer / numbers.length;
+        
+        return answer;
+    }
+}

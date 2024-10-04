b83694ab10c25d8201d3c352c94c2591268a58a8
LastProject <tjdgus5099@naver.com>
2024-10-04 23:58:14 +0900

    문자열 my_string과 정수 n이 매개변수로 주어질 때, my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.

---
+++ sutdy/src/sutdy/s25.java
@@ -0,0 +1,16 @@
+package sutdy;
+
+class s25 {
+	class Solution {
+	    public int solution(int n) {
+	        int answer = 0;
+	        
+	        // n이 0이 될 때까지 반복
+	        while (n > 0) {
+	            answer += n % 10;  // 마지막 자리 숫자를 더함
+	            n = n / 10;        // 마지막 자리를 제거
+	        }
+	        return answer;  // 자리수의 합을 반환
+	    }
+	}
+}

14fecd6c9fe176afcc058b568cdc265ebbf5db16
LastProject <tjdgus5099@naver.com>
2024-10-09 13:51:13 +0900

    34번 정규식 사용 방법 추가 !
    입력: my_string = "a1b2c3"
    
    replaceAll("[^0-9]", ""): 숫자가 아닌 문자들을 제거하여 "123"을 얻음.
    toCharArray(): 문자 배열 {'1', '2', '3'}로 변환.
    Character.getNumericValue(): '1' → 1, '2' → 2, '3' → 3.
    합 계산: 1 + 2 + 3 = 6.
    출력: 6.

---
+++ sutdy/src/sutdy/s34_1.java
@@ -0,0 +1,22 @@
+package sutdy;
+
+class s34_1 {
+	class Solution {
+		public int solution(String my_string) {
+	        int answer = 0;
+	        
+	        // 정규 표현식을 사용하여 my_string에서 숫자가 아닌 문자들을 모두 제거하는 역할 ^는 not임
+	        String str = my_string.replaceAll("[^0-9]","");
+
+	        // str.toCharArray(): 문자열 str을 문자 배열로 변환하여, 하나하나의 문자를 처리할 수 있게 함
+	        // 향상된 for문 사용
+	        // ex) str = "123"이면, {'1', '2', '3'}로 변환
+	        for(char ch : str.toCharArray()) {
+	        	// 각 문자에서 변환된 숫자를 answer에 더해, 최종적으로 모든 숫자의 합을 구함
+	            answer += Character.getNumericValue(ch);
+	        }
+
+	        return answer;
+	    }
+	}
+}

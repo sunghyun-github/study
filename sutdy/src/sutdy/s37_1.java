37ef766d871783e3a2c0a98576f5d61bbf985dc7
LastProject <tjdgus5099@naver.com>
2024-10-11 13:29:23 +0900

    주석달고 추가

--- sutdy/src/sutdy/s37_1.java
+++ sutdy/src/sutdy/s37_1.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 class s37_1 {
 	class Solution {
 	    public int[] solution(int n, int[] numList) {
-	        return Arrays.stream(numList).filter(value -> value % n == 0).toArray();
-	    }
+	        return Arrays.stream(numList)  				// 1. 배열을 스트림으로 변환
+	        		.filter(value -> value % n == 0)	// 2. n으로 나누어 떨어지는 값만 필터링
+	        		.toArray();							// 3. 필터링된 결과를 배열로 변환하여 반환
+	    }	
 	}
 }

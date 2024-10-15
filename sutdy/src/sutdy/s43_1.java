e67153d6cdd94feedf4830f7d2bf1102e5b67371
LastProject <tjdgus5099@naver.com>
2024-10-15 20:12:04 +0900

    일부수정

--- sutdy/src/sutdy/s43_1.java
+++ sutdy/src/sutdy/s43_1.java
@@ -8,8 +8,8 @@ class s43_1 {
 	        // 문자열로 받아서 + 후 long으로 저장 , ex) "12" + "34" = "1234"인데 long로 1234로 저장됨 
 	        // 경우의 수가 많아지면 int로는 메모리가 부족하기 때문에 Long으로 받아줌
 	        // 약간의 편법이지만 String.valueOf(a) 이 부분을 ("" + a + b) 와같이 수정해줄 수 도 있음 아래에 직접 해보겠음. 
-	        long ab = Long.parseLong(String.valueOf(a) + String.valueOf(b));
-	        // long ab = Long.parseLong("" +a+b);
+	        // long ab = Long.parseLong(String.valueOf(a) + String.valueOf(b));
+	        long ab = Long.parseLong("" +a+b);
 	        
 	        long ba = Long.parseLong(String.valueOf(b) + String.valueOf(a));
 	        

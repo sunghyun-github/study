a6c8d857172e5e24256171a5fd3297f98b579a61
LastProject <tjdgus5099@naver.com>
2024-10-16 16:25:02 +0900

    if 문 사용 x 삼항연산 o

---
+++ sutdy/src/sutdy/s44_1.java
@@ -0,0 +1,11 @@
+package sutdy;
+
+class s44_1 {
+	class Solution {
+	    public int solution(int num, int n) {
+	        int answer = num % n == 0 ? 1:0;
+	        
+	        return answer;
+	    }
+	}
+}

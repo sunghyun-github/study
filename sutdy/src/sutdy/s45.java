65b318f1d20900970c88f97beff6652c7c2bc35e
LastProject <tjdgus5099@naver.com>
2024-10-17 10:00:00 +0900

    문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
    append는 문자열을 이어 붙일 때 사용하며, 주로 StringBuilder에서 사용됨
    add는 리스트(ArrayList 등)에 요소를 추가할 때 사용합니다. List, Set, Queue 등의 컬렉션에서 사용되며,

---
+++ sutdy/src/sutdy/s45.java
@@ -0,0 +1,15 @@
+package sutdy;
+
+class s45 {
+	class Solution {
+	    public String solution(String[] arr) {
+	        StringBuilder answer = new StringBuilder();
+	        
+	        for(String str : arr) {
+	            answer.append(str);
+	        }
+	        
+	        return answer.toString();
+	    }
+	}
+}

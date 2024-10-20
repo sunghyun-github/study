49dde15a35196363c4d31bd36fad911ed978d474
LastProject <tjdgus5099@naver.com>
2024-10-20 19:24:07 +0900

    정수 배열 arr와 자연수 k가 주어집니다.
    만약 k가 홀수라면 arr의 모든 원소에 k를 곱하고, k가 짝수라면 arr의 모든 원소에 k를 더합니다.
    
    이러한 변환을 마친 후의 arr를 return 하는 solution 함수를 완성해 주세요.

---
+++ sutdy/src/sutdy/s54.java
@@ -0,0 +1,18 @@
+package sutdy;
+
+
+class s54 {
+	public int[] solution(int[] arr, int k) {
+		int[] answer = {};
+
+		for (int i = 0; i<arr.length; i++) {
+			if (k % 2 == 1 ) {
+				arr[i] *= k;
+			}else if(k % 2 == 0 ) {
+				arr[i] += k;
+			}
+		}
+		answer = arr;
+		return answer;
+	}
+}

7d638879093fc7e60f644f4fdf5c5b5ab9d7fce6
LastProject <tjdgus5099@naver.com>
2024-10-21 12:53:35 +0900

    정규식을 사용한 방법 !

---
+++ sutdy/src/sutdy/s55_1.java
@@ -0,0 +1,21 @@
+package sutdy;
+
+
+class s55_1 {
+	public String solution(String myString) {
+		// StringBuilder는 문자열을 자주 변경(추가, 수정)해야 하는 경우에 유리
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < myString.length(); i++) {
+            char currentChar = myString.charAt(i);
+            
+            if(currentChar < 'l') {
+                answer.append('l');
+            } else {
+                answer.append(currentChar);
+            }
+        }
+        //toString()을 사용해야 최종적으로 문자열(String) 타입으로 반환할 수 있음
+        return answer.toString();
+    }
+}

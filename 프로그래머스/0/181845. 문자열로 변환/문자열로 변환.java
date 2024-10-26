cb0969ad9e1dbc74ae1e3ef7bed17353df7510cd
PSHpp <156566579+sunghyun-github@users.noreply.github.com>
2024-10-26 22:26:19 +0900

    [level 0] Title: 문자열로 변환, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int n) {
+        String answer = String.valueOf(n); // 정수를 문자열로 변환
+        // String answer = Integer.toString(n); // 정수를 문자열로 변환
+        return answer;
+    }
+}

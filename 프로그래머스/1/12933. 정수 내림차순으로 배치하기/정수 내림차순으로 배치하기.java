1ea3519733cd09f7ae5008a3b6911dd67b2f9617
PSHpp <156566579+sunghyun-github@users.noreply.github.com>
2024-11-13 16:43:20 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.53 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -1,27 +1,13 @@
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 class Solution {
-    public long solution(long n) {
-        // long n을 문자열로 변환
-        String str = Long.toString(n);
-        
-        // 문자열을 char 배열로 변환하여 정렬
-        Character[] arr = new Character[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            arr[i] = str.charAt(i);
-        }
-        
-        // 내림차순 정렬
-        Arrays.sort(arr, Collections.reverseOrder());
-        
-        // 정렬된 배열을 문자열로 변환
-        StringBuilder sortedStr = new StringBuilder();
-        for (char c : arr) {
-            sortedStr.append(c);
-        }
-        
-        // 문자열을 long으로 변환하여 반환
-        return Long.parseLong(sortedStr.toString());
-    }
+  public long solution(long n) {
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) sb.append(aList);
+
+        return Long.parseLong(sb.reverse().toString());
+  }
 }

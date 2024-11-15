01145e1fcf763e79728a3e483ffc9d9759adceab
PSHpp <156566579+sunghyun-github@users.noreply.github.com>
2024-11-15 15:40:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int solution(int num) {
-        if (num == 1) return 0; // 입력값이 1인 경우 바로 0 반환
+    public int solution(long num) {
+        int answer = 0;
 
-        long n = num; // long 자료형으로 변경하여 오버플로우 방지
-        int cnt = 0;
-
-        while (cnt < 500) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n = n * 3 + 1;
-            }
-            cnt++;
-            if (n == 1) {
-                return cnt; // n이 1이 되면 작업 횟수 반환
+        while(num != 1){
+            answer++;
+            if(answer==500)
+                return -1;
+            if(num%2==0){
+                num/=2;
+            }else{
+                num=num*3+1;
             }
         }
-
-        return -1; // 500번 반복해도 1이 되지 않으면 -1 반환
+        return answer;
     }
 }

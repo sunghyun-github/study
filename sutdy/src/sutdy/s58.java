b54f1af4bb353ac0ca12c7909683d20757aaa6bd
LastProject <tjdgus5099@naver.com>
2024-11-21 15:00:00 +0900

    asd

---
+++ sutdy/src/sutdy/s58.java
@@ -0,0 +1,16 @@
+package sutdy;
+
+
+class s58 {
+	public int solution(int n, int k) {
+        int answer = 0;
+        int yang = 12000*n;
+        int drink = 2000*k;
+        
+        int freedrink = n / 10;
+        drink -= 2000 * freedrink;
+        
+        return answer = yang+drink;
+        
+    }
+}

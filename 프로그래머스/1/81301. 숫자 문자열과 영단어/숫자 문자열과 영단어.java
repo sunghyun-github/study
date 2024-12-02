f8da8f85b84f21020f806ebb47bfbacedee233f3
PSHpp <156566579+sunghyun-github@users.noreply.github.com>
2024-12-02 17:41:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.10 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String s) {
+        String[] words = {"zero", "one", "two", "three", "four", 
+                          "five", "six", "seven", "eight", "nine"};
+        
+        for (int i = 0; i < words.length; i++) {
+            s = s.replace(words[i], String.valueOf(i)); 
+        }
+        
+        // 변환된 문자열을 정수로 변환
+        return Integer.parseInt(s);
+    }
+}

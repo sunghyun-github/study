ad0d58ba6cd0e509e5997ac9436ff3c822799e8c
PSHpp <156566579+sunghyun-github@users.noreply.github.com>
2024-12-26 21:32:45 +0900

    [level 1] Title: 소수 찾기, Time: 316.27 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,22 +1,18 @@
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        
-        for (int i = 2; i <= n; i++) { 
-            if (isPrime(i)) {
-                answer++; 
-            }
-        }
-        return answer;
-    }
-    
-    private boolean isPrime(int num) {
-        if (num < 2) return false; 
-        for (int i = 2; i * i <= num; i++) { 
-            if (num % i == 0) {
-                return false; 
-            }
-        }
-        return true;
-    }
+  public int solution(int n) {
+      int answer = 0;
+      for(int i = 2; i <= n; i++){
+          int j = 2;
+          int cnt = 0;
+          while(j <= (int)Math.sqrt(i)){
+              if(i % j == 0){
+                cnt += 1;
+                break;
+              } 
+              j += 1;
+          }
+          if(cnt == 0) answer += 1;
+      }
+      return answer;
+  }
 }
